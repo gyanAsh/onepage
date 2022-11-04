5b3798a04f8fe15bec371be6a8873cb608bc6683
gyanAsh <gyanash36@protonmail.com>
2022-11-04 22:53:12 +0530

    chore: Reshaped Initialized

--- next.config.js
+++ next.config.js
@@ -1,6 +1,45 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const loaderUtils = require("loader-utils");
+const withTM = require("next-transpile-modules")(["reshaped"]);
 
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const nextConfig = withTM({
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    const rule = config.module.rules.find((rule) => "oneOf" in rule);
+
+    if (rule && rule.oneOf) {
+      rule.oneOf.forEach((loader) => {
+        if (!Array.isArray(loader.use)) return;
+
+        loader.use.forEach((item) => {
+          if (!item.loader || !item.loader.includes("css-loader")) return;
+
+          item.options = {
+            ...item.options,
+            modules: {
+              ...item.options.modules,
+              // You can customise the way css modules output classnames in your product using:
+              getLocalIdent: (loaderContext, _, localName, options) => {
+                return loaderUtils
+                  .interpolateName(
+                    loaderContext,
+                    `[folder]_[name]__${localName}`,
+                    options
+                  )
+                  .replace(/\.module_/, "_")
+                  .replace(/[^a-zA-Z0-9-_]/g, "_")
+                  .replace(/^(\d|--|-\d)/, "__$1");
+              },
+              mode: "local",
+            },
+          };
+        });
+      });
+    }
+
+    return config;
+  },
+});
+
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "reshaped/themes/reshaped/theme.css";
+import { Reshaped } from 'reshaped';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Reshaped theme="reshaped">
+      <Component {...pageProps} />
+    </Reshaped>
+  )
 }
 
 export default MyApp

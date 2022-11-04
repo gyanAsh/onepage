5ef1483b41a6025a0e204bd46cd6fe586a9be8c8
gyanAsh <gyanash36@protonmail.com>
2022-11-04 22:20:26 +0530

    feat: added congif files

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 120,
+    tabWidth: 2,
+    quoteProps: 'as-needed',
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css',
+    endOfLine: 'lf',
+    arrowParens: 'always',
+    bracketSpacing: true,
+    embeddedLanguageFormatting: 'auto',
+}
